aca11bb813da36538c85b8654280b72c46ceb2be
LeeDark <sergey.povarnin@gmail.com>
2017-08-11 14:28:24 +0300

    exercises chapter 3: 'about' view and URL mapping, adding cross-links index-to-about, about-to-index

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there LeeDark!")
+    return HttpResponse("""
+        Rango says hey there LeeDark! <br/>
+        <a href='/rango/about'>About here</a>""")
+
+
+def about(request):
+    return HttpResponse("""
+        Rango says here is about page. <br/>
+        <a href='/rango'>Index here</a>""")

840b44b4e575c100e09c3f50a1b33a9fd48029dc
LeeDark <sergey.povarnin@gmail.com>
2017-08-16 14:55:21 +0300

    chapter 4.1: using templates

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("""
-        Rango says hey there LeeDark! <br/>
-        <a href='/rango/about'>About here</a>""")
+    context_dict = {
+        'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"
+    }
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -52,10 +51,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ TEMPLATE_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
